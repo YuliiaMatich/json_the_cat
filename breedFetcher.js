28e5e0f620e7473692313e71d3916b27639a2c92
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-07 15:44:26 -0400

    tests added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,20 @@
-const arg = process.argv[2];
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  const data = JSON.parse(body);
-  if (response.statusCode === 404) {
-    console.log("page not found");
-  }
-  if (data.length === 0) {
-    console.log("breed not found");
-  } else {
-    console.log('data', data[0].description);
-    console.log(typeof body);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (response.statusCode === 404) {
+      callback(error, null);
+    }
+    if (data.length === 0) {
+      callback("breed not found", null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
 
-});
+module.exports = { fetchBreedDescription };
